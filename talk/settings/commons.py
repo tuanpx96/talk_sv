b18d5c2346d24f9c70f32f747789cf18f4b9390e
tuanpx96 <tuanpham@tamdongtam.vn>
2018-09-13 16:54:39 +0700

    Update commons.py

--- talk/settings/commons.py
+++ talk/settings/commons.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'talk_db',
         'USER': 'u_talk',
-        'PASSWORD': '1',
+        'PASSWORD': 't',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
